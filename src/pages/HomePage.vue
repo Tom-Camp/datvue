9be06317ab3074af843a2198a3896edf4b9186bd
Tom Camp <tomcamp@protonmail.ch>
2024-12-05 19:28:07 -0500

    typo

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -31,7 +31,7 @@ export default {
     async fetchData() {
       try {
         const apiUrl = import.meta.env.VITE_API_URL
-        const response = await axios.get(`${apiUrl}/items/latest/coop`);
+        const response = await axios.get(`${apiUrl}/items/latest/COOP`);
         this.card = response.data
       } catch (error) {
         console.error('Error fetching data:', error);
